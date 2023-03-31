532ed5af4e18ef15fc82669179d44eb3414847d1
Jordan Bhar <bharjor@sheridancollege.ca>
2023-03-31 13:18:12 -0400

    Initial Commit

---
+++ Assignment_3_DistributedMobility/Assignment_3_DistributedMobilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_3_DistributedMobilityApp.swift
+//  Assignment_3_DistributedMobility
+//
+//  Created by Jordan Bhar on 2023-03-31.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_3_DistributedMobilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
